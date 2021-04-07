0ca6c4e2fb5048099ec5c72dfc76c9e71c88459d
ary123-12 <arianti.sac28@gmail.com>
2021-04-07 13:55:11 -0500

    Version 2

--- Arel/Arel/settings.py
+++ Arel/Arel/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'practica',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Arel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['practica/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
